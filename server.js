4a16bf262fb1bfc1d77d09e40c630b4f2a1c45bf
hegratrix <gratrixmomma@yahoo.com>
2019-01-16 04:20:35 -0800

    deploy issues

--- server.js
+++ server.js
@@ -6,8 +6,10 @@ const path = require('path')
 
 const db = require('./models')
 const app = express()
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/articles"
+const MONGODB_URI = 'mongodb://hegratrix:uciRocks18@ds259154.mlab.com:59154/heroku_fdmr8vk9'|| "mongodb://localhost/articles"
 mongoose.connect(MONGODB_URI)
+.catch(e => 
+    console.log(e))
 
 app.use(logger('dev'))
 app.use(express.static(path.join(__dirname, 'public')))
