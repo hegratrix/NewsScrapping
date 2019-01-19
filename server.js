ec7a44616804033d6cbf8168071609b869566f60
hegratrix <gratrixmomma@yahoo.com>
2019-01-18 18:41:45 -0800

    mongo

--- server.js
+++ server.js
@@ -5,8 +5,9 @@ const bodyparser = require('body-parser')
 const path = require('path')
 
 const app = express()
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines"
-mongoose.connect(MONGODB_URI)
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/articles"
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
 
 app.use(logger('dev'))
 app.use(express.static(path.join(__dirname, 'public')))
@@ -15,4 +16,4 @@ app.use(bodyparser.json())
 
 require('./routes')(app)
 
-app.listen(process.env.PORT || 5000)
+app.listen(process.env.PORT || 3000)
